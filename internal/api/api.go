34b69a23273700852552ad7aa533b38bc4d589e7
Guilherme Torres <guilhermevanderlei0210@gmail.com>
2024-08-07 08:55:31 -0300

    feat: all api routes

--- internal/api/api.go
+++ internal/api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/torressg/go-react-rocketseat/internal/store/pgstore/pgstore"
 )
@@ -25,8 +26,38 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 	a := apiHandler{
 		q: q,
 	}
+
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/rooms", func(r chi.Router) {
+			r.Post("/", a.handleCreateRoom)
+			r.Get("/", a.handleGetRooms)
+
+			r.Route("/{room_id}/messages", func(r chi.Router) {
+				r.Post("/", a.handleCreateRoomMessage)
+				r.Get("/", a.handleGetRoomsMessages)
+
+				r.Route("/{message_id}", func(r chi.Router) {
+					r.Get("/", a.handleGetRoomMessage)
+					r.Patch("/react", a.handleReactionToMessage)
+					r.Delete("/react", a.handleRemoveReactionFromMessage)
+					r.Patch("/answer", a.handleMarkMessageAsAnswered)
+				})
+			})
+		})
+	})
 
 	a.r = r
 	return a
 }
+
+func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request)                {}
+func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request)                  {}
+func (h apiHandler) handleGetRoomsMessages(w http.ResponseWriter, r *http.Request)          {}
+func (h apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request)         {}
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request)            {}
+func (h apiHandler) handleReactionToMessage(w http.ResponseWriter, r *http.Request)         {}
+func (h apiHandler) handleRemoveReactionFromMessage(w http.ResponseWriter, r *http.Request) {}
+func (h apiHandler) handleMarkMessageAsAnswered(w http.ResponseWriter, r *http.Request)     {}
